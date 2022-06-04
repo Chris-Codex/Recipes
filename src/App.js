fab282cc661c25f94e9324497fb093e6c3c935c1
Christian Obinna Iheacho <christianobinna.iheacho@student.griffith.ie>
2022-06-04 09:16:07 +0100

    Implementation of hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Axios from "axios";
 
 function App() {
+  // const YOUR_APP_ID = "9162e998";
+  // const YOUR_APP_KEY = "8a4076f902f506b4607f8305ce90b1ff";
+
+  const [query, setQuery] = useState("");
+
+  let url =
+    "https://api.edamam.com/search?q=chicken&app_id=9162e998&app_key=8a4076f902f506b4607f8305ce90b1ff&&health=alcohol-free";
+
+  async function getRecipes() {
+    let result = await Axios.get(url);
+    console.log(result.data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 onClick={getRecipes}>Food Recipe Plaza</h1>
+      <form className="app__searchForm">
+        <input type="text" placeholder="enter ingrident" value={query} />
+      </form>
     </div>
   );
 }
